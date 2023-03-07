214187827f6e07b50404a704a7e53fbcd66b5526
norazics <nora.zics@yahoo.co.uk>
2023-03-07 12:20:04 +0000

    finished authentication

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:vip]
+
   def home
+  end
+
+  def extrainfo
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   get 'pages/home'
+  get 'pages/extrainfo'
+
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
