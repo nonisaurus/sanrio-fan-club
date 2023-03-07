0891d3fc6cb56b6baf151a68dab8daf65b14112d
norazics <nora.zics@yahoo.co.uk>
2023-03-07 12:44:44 +0000

    seeded some users and characters

--- app/models/character_card.rb
+++ app/models/character_card.rb
@@ -5,23 +5,13 @@ class CharacterCard < ApplicationRecord
   validates :name, length: {minimum: 3, too_short: "Name has to be at least %{count} characters long"}
 
 
-  def is_protected?
-        protected
-  end
-
-
-  # Registers a callback to be called after a record is created
-  after_create :check_user_id
+  before_destroy :check_if_protected
 
   private
 
-  def check_user_id
-    # if the user_id of the newly created cards matches the id of the current user
-    unless self.user_id == Current.user.id
-      self.destroy
-      # if it doesnt match then displays an error
-      errors.add(:user_id, "User ID does not match current user")
-      # a way to stop the execution of a callback and immediately exit from the current method
+  def check_if_protected
+    if self.protected?
+      errors.add(:protected, "This card is protected and cannot be deleted.")
       throw(:abort)
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create( first_name: "noni",  last_name: "summers",   username: "noni", email: "noni@test.com", password: "123456789")
+User.create( first_name: "pickles",   last_name: "summers",   username: "pickles", email: "pickles@test.com" , password: "123456789")
+
+CharacterCard.create!(user_id: User.first.id, name: "Hello Kitty",  birthday: "1 November", location: "London, England",   fav_food: "Apple pie",  fave_colour: "red",   image: "https://upload.wikimedia.org/wikipedia/en/0/05/Hello_kitty_character_portrait.png",   protected: true)
+CharacterCard.create!(user_id: User.first.id, name: "Dear Daniel",   birthday: "August 25", location: "London, England",  fav_food: "apple pie",  fave_colour: "blue",  image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Dear_Daniel_character_portrait.png/220px-Dear_Daniel_character_portrait.png", protected: true)
