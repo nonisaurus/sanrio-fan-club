9711cd751e891bcab308d78887f762a8fcf851ff
norazics <nora.zics@yahoo.co.uk>
2023-03-08 11:51:46 +0000

    working on CRUB - create

--- app/controllers/character_cards_controller.rb
+++ app/controllers/character_cards_controller.rb
@@ -1,36 +1,41 @@
 class CharacterCardsController < ApplicationController
-    before_action :authenticate_user!, only: [:cards, :show]
+    before_action :authenticate_user!, only: [:cards, :show, :new, :create, :delete]
 
     def cards
         @character_cards = CharacterCard.all
     end
 
     def show
-        @character_card = CharacterCard.find(params[:id])
+        if params[:id] == "new"
+            redirect_to new_character_card_path
+        else
+            @character_card = CharacterCard.find(params[:id])
+        end
     end
   
     def new
-      @character_card = CharacterCard.new
+        @character_card = CharacterCard.new
     end
   
     def edit
     end
   
     def create
-      @character_card = CharacterCard.new()
-      if @character_card.save
-        redirect_to @character_card, notice: 'Character card was successfully created.'
-      else
-        render :new
-      end
+        @character_card = CharacterCard.new(character_card_params)
+
+        if @character_card.save
+            redirect_to @character_card
+        else
+            render :new
+        end
     end
   
     def update
-      if @character_card.update()
-        redirect_to @character_card, notice: 'Character card was successfully updated.'
-      else
-        render :edit
-      end
+        if @character_card.update()
+            redirect_to @character_card, notice: 'Character card was successfully updated.'
+        else
+            render :edit
+        end
     end
 
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,12 @@ Rails.application.routes.draw do
 
   get 'characters', to:'character_cards#cards'
   get 'characters/:id', to:'character_cards#show'
-  get 'character/new', to: 'character_cards#new'
+  get 'character/new', to: 'character_cards#new', as: 'new_character_card'
   get 'character', to: 'character_cards#create'
   get 'character/:id/edit', to: 'character_cards#edit'
   patch 'character/:id', to: 'character_cards#update'
   delete 'character/:id', to: 'character_cards#destroy'
-
-
-
-
+  
 
   get 'pages/home'
   get 'pages/extrainfo'
